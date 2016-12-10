7473eefc35039fced2a6a004b02836ff724881c0
Eric Liu <eric.liu.developer@gmail.com>
2016-12-10 11:29:18 +1100

    - working state

--- app/src/main/java/com/example/ericliu/viewrefresh/MainActivity.java
+++ app/src/main/java/com/example/ericliu/viewrefresh/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.ericliu.viewrefresh;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -17,44 +17,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         viewRefreshHandler = new ViewRefreshHandler();
 
-        final TextView tvMain = (TextView) findViewById(R.id.tvMain);
-        Button btnRefresh = (Button) findViewById(R.id.btnRefresh);
+        final TextView tvCounter = (TextView) findViewById(R.id.tvCounter);
+        Button btnRefresh = (Button) findViewById(R.id.btnStartCounter);
 
         btnRefresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                viewRefreshHandler.executePeriodically(new RefreshRunnalbe(tvMain, null), 1000);
+                viewRefreshHandler.executePeriodically(new CountUpRunnalbe(tvCounter, null), 1000);
             }
         });
 
 
-        Button btnNextActivity = (Button) findViewById(R.id.btnNextActivity);
+        final TextView tvTimer = (TextView) findViewById(R.id.tvTimer);
+        Button btnNextActivity = (Button) findViewById(R.id.btnStartTimer);
         btnNextActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, PlaceHolderActivity.class));
+                viewRefreshHandler.executePerSecond(new TimerRunnable(tvTimer, null));
             }
         });
     }
 
 
     /**
-     *  uses a static inner-class to avoid the runnable object holding a reference to the activity.
+     * uses a static inner-class to avoid the runnable object holding a reference to the activity.
      */
-    private static class RefreshRunnalbe extends ViewRefreshHandler.ViewRunnable<TextView> {
-        private static final String ARGS_COUNT = "args.count";
+    private static class CountUpRunnalbe extends ViewRefreshHandler.ViewRunnable<TextView> {
+        private int count;
 
-        RefreshRunnalbe(TextView textView, Bundle args) {
+        CountUpRunnalbe(TextView textView, Bundle args) {
             super(textView, args);
         }
 
         @Override
         protected void run(TextView textView, Bundle args) {
-            int count = args.getInt(ARGS_COUNT);
-            textView.setText(count + " ");
+            textView.setText(count + "");
+            count++;
+        }
+    }
 
-            args.putInt(ARGS_COUNT, ++count);
+
+    private static class TimerRunnable extends ViewRefreshHandler.ViewRunnable<TextView> {
+        private int second = 30;
+
+        public TimerRunnable(TextView view, @Nullable Bundle args) {
+            super(view, args);
+        }
+
+        @Override
+        protected void run(TextView textView, Bundle args) {
+            if (second == 0) {
+                terminate();
+            }
+            textView.setText(second + "");
+            second--;
         }
     }
 }
+
 
--- app/src/main/java/com/example/ericliu/viewrefresh/ViewRefreshHandler.java
+++ app/src/main/java/com/example/ericliu/viewrefresh/ViewRefreshHandler.java
@@ -17,6 +17,7 @@ import java.lang.ref.WeakReference;
 
 public final class ViewRefreshHandler {
     private static final long MINI_SECS_ONE_MINUTE = 1000 * 60;
+    private static final long MINI_SECS_ONE_SECOND = 1000;
 
     private final Handler mHandler;
     private Runnable mRunnableDecorator;
@@ -25,6 +26,9 @@ public final class ViewRefreshHandler {
         mHandler = new Handler(Looper.getMainLooper());
     }
 
+    public void executePerSecond(ViewRunnable runnable) {
+        executePeriodically(runnable, MINI_SECS_ONE_SECOND);
+    }
 
     public void executePerMinute(ViewRunnable runnable) {
         executePeriodically(runnable, MINI_SECS_ONE_MINUTE);
@@ -42,7 +46,7 @@ public final class ViewRefreshHandler {
             @Override
             public void run() {
                 runnable.run();
-                if (runnable.viewRef.get() != null) {
+                if (runnable.viewRef.get() != null && !runnable.terminate) {
                     scheduleNext(interval);
                 } else {
                     // stop refreshing when the View is gone.
@@ -50,7 +54,7 @@ public final class ViewRefreshHandler {
                 }
             }
         };
-        scheduleNext(interval);
+        mHandler.post(mRunnableDecorator);
     }
 
     private void cancelPendingTask() {
@@ -76,6 +80,7 @@ public final class ViewRefreshHandler {
     public static abstract class ViewRunnable<T extends View> implements Runnable {
         private final WeakReference<T> viewRef;
         private final Bundle mArgs;
+        private boolean terminate = false; // set true to stop executing repeating tasks
 
         public ViewRunnable(T view, @Nullable Bundle args) {
             viewRef = new WeakReference<>(view);
@@ -92,6 +97,10 @@ public final class ViewRefreshHandler {
             if (view != null) {
                 run(view, mArgs);
             }
+        }
+
+        protected void terminate() {
+            terminate = true;
         }
 
         protected abstract void run(T view, Bundle args);
